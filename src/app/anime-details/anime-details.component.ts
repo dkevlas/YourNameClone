73a2dc98b1019cd042295afc0baadba87daa280d
Kevin <dennisk.blas@gmail.com>
2024-09-28 19:14:58 -0500

    se creó componente secundario para la seccion anime-detailsjuntos con sus componentes hijo e estilos

---
+++ src/app/anime-details/anime-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-details',
+  templateUrl: './anime-details.component.html',
+  styleUrl: './anime-details.component.scss'
+})
+export class AnimeDetailsComponent {
+
+}
