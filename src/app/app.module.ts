24d4cbe8b18914f9cc9d9e79cdce7906554e15a5
Kevin <dennisk.blas@gmail.com>
2024-09-28 19:11:33 -0500

    se agregó todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { AnimeDetailsComponent } from './anime-details/anime-details.component';
+import { NameComponent } from './banner/name/name.component';
+import { NavComponent } from './banner/nav/nav.component';
+import { SummaryComponent } from './anime-details/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    AnimeDetailsComponent,
+    NameComponent,
+    NavComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
